8e03b36fdfc4611b2263d2d96932c360877cb956
HONGJIBEOM <corea1836@naver.com>
2023-03-23 23:31:53 +0900

    test

--- src/main/java/test/controller/Sample.java
+++ src/main/java/test/controller/Sample.java
@@ -3,11 +3,28 @@ package test.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @RestController
 public class Sample {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World4 ";
+        String result = null;
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://27.96.135.218:3306/test",
+                    "readonly",
+                    "0000"
+            );
+            if(con != null) {
+                result = con.toString();
+            }
+        } catch (SQLException e) {
+            result = e.toString();
+        }
+        return result;
     }
 }
